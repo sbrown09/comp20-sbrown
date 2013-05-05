3ce8f339939eea047d86bfd274488ae8213686ff
SAMMY BROWN <sbrown09@tufts.edu>
2013-05-05 18:58:28 -0400

    lat vs long

--- where/where.js
+++ where/where.js
@@ -201,10 +201,10 @@ var lon1 = Stations[i]['kb'];
 var R = 6371; // km 
 //has a problem with the .toRad() method below.
 var x1 = lat2-lat1;
-var dLat = x1.toRad();  
+var dLat = x1*Math.PI / 180;  
 var x2 = lon2-lon1;
-var dLon = x2.toRad(); 
-var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * Math.sin(dLon/2) * Math.sin(dLon/2);  
+var dLon = x2*Math.PI / 180; 
+var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1*Math.PI / 180 * Math.cos(lat2*Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);  
 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 var d = R * c; 
 var closest;
