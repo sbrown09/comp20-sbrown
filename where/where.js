4879ba3ae2b9e6c4e179ab190b5c907047ec9edc
SAMMY BROWN <sbrown09@tufts.edu>
2013-05-05 19:02:02 -0400

    lat vs long

--- where/where.js
+++ where/where.js
@@ -204,7 +204,7 @@ var x1 = lat2-lat1;
 var dLat = x1*Math.PI / 180;  
 var x2 = lon2-lon1;
 var dLon = x2*Math.PI / 180; 
-var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1*Math.PI / 180 * Math.cos(lat2*Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);  
+var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1*Math.PI / 180) * Math.cos(lat2*Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);
 var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
 var d = R * c; 
 var closest;
